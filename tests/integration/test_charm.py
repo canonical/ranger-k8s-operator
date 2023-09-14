6d85c79087858e965c1bfe464b3971f881ec5066
Amber <amber.charitos@canonical.com>
2023-09-14 13:28:01 +0100

    add eof and ingress integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,11 +5,13 @@
 """Charm integration tests."""
 
 import logging
+import socket
+import unittest.mock
 
 import pytest
 import requests
 from conftest import deploy  # noqa: F401, pylint: disable=W0611
-from helpers import APP_NAME, get_unit_url
+from helpers import APP_NAME, gen_patch_getaddrinfo, get_unit_url
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
@@ -29,3 +31,23 @@ class TestDeployment:
 
         response = requests.get(url, timeout=300, verify=False)  # nosec
         assert response.status_code == 200
+
+    async def test_ingress(self, ops_test: OpsTest):
+        """Set external-hostname and test connectivity through ingress."""
+        new_hostname = "ranger-admin"
+        application = ops_test.model.applications[APP_NAME]
+        await application.set_config({"external-hostname": new_hostname})
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME, "nginx-ingress-integrator"],
+            status="active",
+            raise_on_blocked=False,
+            timeout=600,
+        )
+        with unittest.mock.patch.multiple(
+            socket,
+            getaddrinfo=gen_patch_getaddrinfo(new_hostname, "127.0.0.1"),
+        ):
+            response = requests.get(
+                f"https://{new_hostname}", timeout=300, verify=False
+            )  # nosec
+            assert response.status_code == 200

6891206f59443764bdcebd0a6191a8206cb43d2b
Amber <amber.charitos@canonical.com>
2023-09-14 14:37:12 +0100

    removing ingress test - creating separate PR

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -5,7 +5,6 @@
 """Charm integration test helpers."""
 
 import logging
-import socket
 from pathlib import Path
 
 import yaml
@@ -60,32 +59,3 @@ async def get_unit_url(
         f"{application}/{unit}"
     ]["address"]
     return f"{protocol}://{address}:{port}"
-
-
-def gen_patch_getaddrinfo(host: str, resolve_to: str):  # noqa
-    """Generate patched getaddrinfo function.
-
-    This function is used to generate a patched getaddrinfo function that will resolve to the
-    resolve_to address without having to actually register a host.
-
-    Args:
-        host: intended hostname of a given application.
-        resolve_to: destination address for host to resolve to.
-    Returns:
-        A patching function for getaddrinfo.
-    """
-    original_getaddrinfo = socket.getaddrinfo
-
-    def patched_getaddrinfo(*args):
-        """Patch getaddrinfo to point to desired ip address.
-
-        Args:
-            args: original arguments to getaddrinfo when creating network connection.
-        Returns:
-            Patched getaddrinfo function.
-        """
-        if args[0] == host:
-            return original_getaddrinfo(resolve_to, *args[1:])
-        return original_getaddrinfo(*args)
-
-    return patched_getaddrinfo
